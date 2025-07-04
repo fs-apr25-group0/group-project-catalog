2263139c65901b0cc9507da6c54b73d4b4be4e46
Vitalii Storcheus <storcheusvitalii@gmail.com>
2025-07-04 18:04:27 +0300

    fix name function getAccessories

--- src/api/fetchAccessories/fetchAccessories.ts
+++ src/api/fetchAccessories/fetchAccessories.ts
@@ -6,7 +6,7 @@ function wait(delay: number) {
   return new Promise((resolve) => setTimeout(resolve, delay));
 }
 
-export async function getPeople(): Promise<Accessory[]> {
+export async function getAccessories(): Promise<Accessory[]> {
   return wait(500)
     .then(() => fetch(API_URL))
     .then((response) => response.json());
