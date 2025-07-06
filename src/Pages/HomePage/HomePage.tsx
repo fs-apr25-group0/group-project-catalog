29815c543229c3ec6e0aaa1d345037fbcc201359
Viktoria Zabolotnia <vika.zab4@gmail.com>
2025-07-06 20:32:57 +0300

    NGT4-18/implement_a_product_card_for_the_cart

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,12 +1,3 @@
-import { Dropdown } from '../../Components/Dropdown';
-
 export const HomePage = () => {
-  return (
-    <>
-      <Dropdown />
-
-      <h1>BOOO</h1>
-    </>
-  );
-  // return <p>You are on home page</p>;
+  return <p>You are on home page</p>;
 };
