b7dcce583c46161ac96ba2fe1f068d3dc3b0c360
Vitalii Storcheus <storcheusvitalii@gmail.com>
2025-07-09 00:00:03 +0300

    internacionalization

--- src/Pages/ProductInfoPage/ProductInfoPage.tsx
+++ src/Pages/ProductInfoPage/ProductInfoPage.tsx
@@ -58,6 +58,8 @@ export const ProductInfoPage = () => {
       <SliderForProduct
         visibleProducts={visibleMayLikeProducts}
         title={'You may also like'}
+        productType={'phones'}
+        sortBy={'newest'}
       />
     </main>
   );
