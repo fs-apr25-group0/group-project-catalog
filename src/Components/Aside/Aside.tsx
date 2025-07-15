a2a3cd83aaee685516fede7ee4bbc955e676a896
YarRudenko <Yarik18rudenko@gmail.com>
2025-07-15 18:15:52 +0300

    add fix for header and realized dark mode for header

--- src/Components/Aside/Aside.tsx
+++ src/Components/Aside/Aside.tsx
@@ -1,10 +1,12 @@
+import { useThemeState } from '../../stateManagers/themeState';
 import { NavigationCartFavorite } from '../NavigationCartFavorite';
 import { NavigationMain } from '../NavigationMain';
 import './Aside.scss';
 
 export const Aside = () => {
+  const { theme } = useThemeState();
   return (
-    <aside className="aside">
+    <aside className={`aside aside--${theme}`}>
       <nav className="aside__nav-bar">
         <NavigationMain isAside={true} />
         <NavigationCartFavorite isAside={true} />
