3bd32e47ac7fe649869368bc256a5b587d79a8a7
Viktoria Zabolotnia <vika.zab4@gmail.com>
2025-07-16 22:06:45 +0300

    change color for skeleton

--- src/Components/SkeletonProductCard/SkeletonProductCard.tsx
+++ src/Components/SkeletonProductCard/SkeletonProductCard.tsx
@@ -1,49 +1,57 @@
-import Skeleton from 'react-loading-skeleton';
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 import './SkeletonProductCard.scss';
+import { useThemeState } from '../../stateManagers/themeState';
 
 export const SkeletonProductCard = () => {
+  const { theme } = useThemeState();
+
   return (
-    <article className={`skeletonCard`}>
-      <div className="skeletonCard__image">
-        <Skeleton
-          height={196}
-          width="100%"
-        />
-      </div>
+    <SkeletonTheme
+      baseColor={theme === 'dark' ? '#161827' : '#efefefff'}
+      highlightColor={theme === 'dark' ? '#1a1c2eff' : '#e2e2e2ff'}
+    >
+      <article className={`skeletonCard`}>
+        <div className="skeletonCard__image">
+          <Skeleton
+            height={196}
+            width="100%"
+          />
+        </div>
 
-      <div className="skeletonCard__title">
-        <Skeleton
-          height={20}
-          count={2}
-        />
-      </div>
+        <div className="skeletonCard__title">
+          <Skeleton
+            height={20}
+            count={2}
+          />
+        </div>
 
-      <div className="skeletonCard__price">
-        <Skeleton height={35} />
-      </div>
+        <div className="skeletonCard__price">
+          <Skeleton height={35} />
+        </div>
 
-      <hr className="skeletonCard__divider" />
+        <hr className="skeletonCard__divider" />
 
-      <div className="skeletonCard__specs">
-        <Skeleton
-          height={10}
-          count={3}
-        />
-      </div>
+        <div className="skeletonCard__specs">
+          <Skeleton
+            height={10}
+            count={3}
+          />
+        </div>
 
-      <div className="skeletonCard__buttons">
-        <Skeleton
-          height={40}
-          width={160}
-          style={{ borderRadius: 48 }}
-        />
-        <Skeleton
-          height={40}
-          width={40}
-          style={{ borderRadius: 48 }}
-        />
-      </div>
-    </article>
+        <div className="skeletonCard__buttons">
+          <Skeleton
+            height={40}
+            width={160}
+            style={{ borderRadius: 48 }}
+          />
+          <Skeleton
+            height={40}
+            width={40}
+            style={{ borderRadius: 48 }}
+          />
+        </div>
+      </article>
+    </SkeletonTheme>
   );
 };
